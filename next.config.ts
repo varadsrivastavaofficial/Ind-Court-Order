dede158ebd9e759901360d6cdbb6d5ff00e2c4cc
Varad Srivastava (Services) <varadsrivastava7@gmail.com>
2026-01-28 17:06:28 +0000

    error while deploying
    Run npm run build
    
    > nextn@0.1.0 build
    > NODE_ENV

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type {NextConfig} from 'next';
 
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  // The basePath and assetPrefix are configured for GitHub Pages.
-  // The repository name is hardcoded here. If you change your repository name, you'll need to update it.
-  basePath: isGithubActions ? '/nextn' : '',
-  assetPrefix: isGithubActions ? '/nextn/' : '',
-  
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -16,7 +8,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    // Disabling image optimization is required for static export to GitHub Pages.
     unoptimized: true,
     remotePatterns: [
       {
